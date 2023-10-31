ed3e219175bc8f7e6aac3df6ce384f249a24d3d0
Vojan-Najov <serejah.mashichev@gmail.com>
2023-10-31 03:29:07 +0300

    Make gen_dst_pathname as static.

--- src/zip/main.c
+++ src/zip/main.c
@@ -6,7 +6,17 @@
 #include "args.h"
 #include "compressor.h"
 
+/* макросы для размера статического буфера, 4 символа для расширения ".zip" */
+
+#define BUFFER_SIZE 1024
+#define CHARS_FOR_EXT 4
+#define EXT_CHARS ".zip"
+
+/* Объвления вспомогательных статических функций */
+
 static int close_stream(FILE *stream, const char *pathname);
+
+static char *generate_dst_pathname(const char *src_pathname);
 
 int main(int argc, char *argv[])
 {
@@ -49,6 +59,12 @@ int main(int argc, char *argv[])
 	return err_status ? EXIT_FAILURE : EXIT_SUCCESS;
 }
 
+/*
+*  close_stream
+*
+*  Закрытие потока с обработкой ошибки.
+*/
+
 static int close_stream(FILE *stream, const char *pathname)
 {
 	if (fclose(stream)) {
@@ -56,4 +72,36 @@ static int close_stream(FILE *stream, const char *pathname)
 		return EXIT_FAILURE;
 	}
 	return EXIT_SUCCESS;
+}
+
+/*
+*  generate_dst_pathname
+*
+*  Функция генерирует имя выходного файла, если таковой не указан пользователем,
+*  отсекая всё, кроме имени входного файла, и добавляя расширение ".zip"
+*  Функция по сути явяется одноразовой, поэтому вместо нормальной аллокации на куче,
+*  имя файла записывается в статичный буфер размера 1 кб. Если входное имя столь
+*  велико, что не помещается, то она урезается до такой длины, чтобы уместить еще и
+*  символы для расширения.
+*/
+
+static char *generate_dst_pathname(const char *src_pathname)
+{
+	int idx;
+	static char buffer[BUFFER_SIZE];
+
+	if (*src_pathname == '\0') {
+		return NULL;
+	}
+
+	idx = (int) strlen(src_pathname) - 1;
+	while (idx >= 0 && src_pathname[idx] != '/') {
+		--idx;
+	}
+	++idx;
+
+	strncpy(buffer, src_pathname + idx, BUFFER_SIZE - CHARS_FOR_EXT - 1);
+	strcat(buffer, EXT_CHARS);
+
+	return buffer;
 }
